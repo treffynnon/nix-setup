33719e8d7a79f60165256913b65757dc61c09270
Simon Holywell <simon@holywell.com.au>
2019-10-17 22:52:54 +1000

    fixes

--- example-shell.nix
+++ example-shell.nix
@@ -1,8 +1,13 @@
 let
-  nix1809 = import (builtins.fetchGit {
+  # nix1809 = import (builtins.fetchGit {
+  #   name = "nixos-18_09";
+  #   url = https://github.com/NixOS/nixpkgs.git;
+  #   ref = "refs/tags/18.09";
+  # }) {};
+  nix1809 = import (builtins.fetchTarball {
     name = "nixos-18_09";
-    url = https://github.com/NixOS/nixpkgs.git;
-    ref = "refs/tags/18.09";
+    url = https://github.com/NixOS/nixpkgs/archive/18.09.tar.gz;
+    sha256 = "1ib96has10v5nr6bzf7v8kw7yzww8zanxgw2qi1ll1sbv6kj6zpd";
   }) {};
 in
 
--- home-configs/direnv.nix
+++ home-configs/direnv.nix
@@ -1,9 +1,8 @@
 let
-  nixDirenvPath = (builtins.fetchGit {
+  nixDirenvPath = (builtins.fetchTarball {
     name = "nix-direnv";
-    url = https://github.com/nix-community/nix-direnv.git;
-    rev = "e7df598cd0addefb51b678a2a4b1607942f153e7";
-    ref = "master";
+    url = https://github.com/nix-community/nix-direnv/archive/e7df598cd0addefb51b678a2a4b1607942f153e7.tar.gz;
+    sha256 = "0bj561c0qawadd62n1xidxkv6zdmdb39zhyvl2l9x2l62j1abriw";
   });
 in
 {
