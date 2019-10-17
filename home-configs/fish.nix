278ab057c299cd72486dc29630d4704686cd2b31
Simon Holywell <simon@holywell.com.au>
2019-10-17 10:23:38 +1000

    working to get fzf linked into fish properly

--- home-configs/fish.nix
+++ home-configs/fish.nix
@@ -1,3 +1,20 @@
+{ lib, ... }:
+
+let
+  fishFzfKeybindings = (builtins.fetchGit {
+    url = https://github.com/jethrokuan/fzf.git;
+    rev = "ac01d96fc6344ebeb48c03f2c9c0be5bf3b20f1c";
+    ref = "master";
+  });
+  basePath = /. + ("/" + ((builtins.unsafeDiscardStringContext fishFzfKeybindings) + "/functions"));
+  fzfFunctions = with builtins;
+    lib.concatMapStringsSep
+      "\n"
+      (x: readFile basePath + ("/" + x))
+      (filter
+        (x: match ".*\\.fish" != null)
+        (attrNames (readDir basePath)));
+in
 {
   programs.fish = {
     enable = true;
@@ -9,4 +26,6 @@
     source = ./fish/functions;
     recursive = true;
   };
+
+  xdg.configFile."fish/functions/___fzf.fish".text = fzfFunctions;
 }
