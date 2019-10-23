847e3efe229e229507879a9487c4d336397d8b04
Simon Holywell <simon@holywell.com.au>
2019-10-23 16:31:57 +1000

    improve hammerspoon config a bit

--- home-configs/hammerspoon/init.lua
+++ home-configs/hammerspoon/init.lua
@@ -1,19 +1,30 @@
 local hyper = {"ctrl", "alt", "cmd"}
+hs.window.animationDuration = 0.2
 
+-- Load some spoons from ~/.hammerspoon/Spoons
 hs.loadSpoon("MiroWindowsManager")
 hs.loadSpoon("WindowScreenLeftAndRight")
+
+-- Tap control for `escape` and hold for `control`
 hs.loadSpoon('ControlEscape'):start()
 
-hs.window.animationDuration = 0.1
+-- A little window manager for pushing around windows with
+-- the keyboard
 spoon.MiroWindowsManager:bindHotkeys({
   up = {hyper, "up"},
   right = {hyper, "right"},
   down = {hyper, "down"},
   left = {hyper, "left"},
-  fullscreen = {hyper, "f"}
+  fullscreen = {hyper, "space"}
 })
 
+-- Move windows from screen to screen
 spoon.WindowScreenLeftAndRight:bindHotkeys({
   screen_left = { hyper, "," },
   screen_right= { hyper, "." },
 })
+
+-- hs.hotkey.bind
+-- if hs.window.focusedWindow() then
+--   hs.window.frontmostWindow().toggleZoom()
+-- end
