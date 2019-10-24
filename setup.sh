25edaadadfac668cbbb46fbbe9db0bfd19f19098
Simon Holywell <simon@holywell.com.au>
2019-10-24 11:27:03 +1000

    improve the documentation

--- setup.sh
+++ setup.sh
@@ -11,6 +11,8 @@ nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
 nix-channel --add https://github.com/rycee/home-manager/archive/release-19.03.tar.gz home-manager
 nix-channel --update
 
-mv /etc/shells /etc/shells.bak
-mv /etc/zprofile /etc/zprofile.local
-mv /etc/zshrc /etc/zshrc.local
+sudo mv /etc/shells /etc/shells.bak
+sudo mv /etc/zprofile /etc/zprofile.local
+sudo mv /etc/zshrc /etc/zshrc.local
+
+darwin-rebuild switch
