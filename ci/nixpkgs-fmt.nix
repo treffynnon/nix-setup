4bb99032aa63a61b9e53a5d8e49078acee56a9cc
Simon Holywell <simon@holywell.com.au>
2019-10-28 13:51:05 +1000

    nixpkgs-fmt

--- ci/nixpkgs-fmt.nix
+++ ci/nixpkgs-fmt.nix
@@ -1,16 +1,8 @@
-let
-  nixMaster = import (
+{ pkgs ? import (
     builtins.fetchTarball {
       name = "nix-master-25fb1e59d6";
       url = https://github.com/NixOS/nixpkgs/archive/25fb1e59d61b735f2e7c759c37feb449ce7eb8ae.tar.gz;
     }
-  ) {};
-in
-
-  with import <nixpkgs> {};
-  stdenv.mkDerivation {
-    name = "nix-setup-ci";
-    buildInputs = [
-      nixMaster.nixpkgs-fmt
-    ];
-  }
+  ) {}
+}:
+pkgs.nixpkgs-fmt
