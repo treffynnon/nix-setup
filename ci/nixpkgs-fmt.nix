3ca0111c47c344e58a86d9860a95294b8df87f36
Simon Holywell <simon@holywell.com.au>
2019-10-29 16:39:47 +1000

    add hashes to tarball fetches

--- ci/nixpkgs-fmt.nix
+++ ci/nixpkgs-fmt.nix
@@ -2,6 +2,7 @@
     builtins.fetchTarball {
       name = "nix-master-25fb1e59d6";
       url = https://github.com/NixOS/nixpkgs/archive/25fb1e59d61b735f2e7c759c37feb449ce7eb8ae.tar.gz;
+      sha256 = "0bxb2qlp9cf1g78pa2mqllp7d0s0n8ypr7m39q3rl76adlmkr8qa";
     }
   ) {}
 }:
