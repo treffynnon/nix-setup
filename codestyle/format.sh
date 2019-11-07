e713135a6bdeb2e556e9a41ff6e82f38491e6efc
Simon Holywell <simon@holywell.com.au>
2019-11-07 21:59:26 +1000

    patch paths in format script

--- codestyle/format.sh
+++ codestyle/format.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
+NIXPKGS_BASEPATH=$(realpath `dirname "$0"`/../)
+echo $NIXPKGS_BASEPATH
+
 echo "Formatting Hammerspoon lua code"
-find ../home-configs/hammerspoon -type f -name "*.lua" -exec lua-format -c ./lua-format-config.yml {} +
+find "$NIXPKGS_BASEPATH/home-configs/hammerspoon" -type f -name "*.lua" -exec lua-format -c "$NIXPKGS_BASEPATH/codestyle/lua-format-config.yml" {} +
 
 echo "Formatting nix files"
-nixpkgs-fmt --check ../**/*.nix
+nixpkgs-fmt --check "$NIXPKGS_BASEPATH"/**/*.nix
 
 echo "Linting lua code"
 echo "Not yet implemented"
 
 echo "Linting nix files"
-nix-linter ../**/*.nix
+nix-linter "$NIXPKGS_BASEPATH"/**/*.nix
