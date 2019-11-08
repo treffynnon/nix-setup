773ac53cef341117cdd28d936694c11991cc6d43
Simon Holywell <simon@holywell.com.au>
2019-11-08 14:26:05 +1000

    luacheck install using nix

--- codestyle/format.sh
+++ codestyle/format.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 NIXPKGS_BASEPATH=$(realpath `dirname "$0"`/../)
-echo $NIXPKGS_BASEPATH
 
 echo "Formatting Hammerspoon lua code"
 find "$NIXPKGS_BASEPATH/home-configs/hammerspoon" -type f -name "*.lua" -exec lua-format -c "$NIXPKGS_BASEPATH/codestyle/lua-format-config.yml" {} +
+echo "Done."
+echo " "
 
 echo "Formatting nix files"
-nixpkgs-fmt --check "$NIXPKGS_BASEPATH"/**/*.nix
+nixpkgs-fmt "$NIXPKGS_BASEPATH"/**/*.nix
+echo "Done."
+echo " "
 
 echo "Linting lua code"
-echo "Not yet implemented"
+luacheck --globals hs spoon --no-max-line-length "$NIXPKGS_BASEPATH/home-configs/hammerspoon"
+echo "Done."
+echo " "
 
 echo "Linting nix files"
 nix-linter "$NIXPKGS_BASEPATH"/**/*.nix
+echo "Done."
+echo " "
