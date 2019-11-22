f59d76c432339000ffb98c38d0d9c1361794833a
Simon Holywell <simon@holywell.com.au>
2019-11-22 12:11:15 +1000

    use newly working busted in nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,9 +1,9 @@
 let
   pkgs = import (
     builtins.fetchTarball {
-      name = "nix-master-25fb1e59d6";
-      url = https://github.com/NixOS/nixpkgs/archive/25fb1e59d61b735f2e7c759c37feb449ce7eb8ae.tar.gz;
-      sha256 = "0bxb2qlp9cf1g78pa2mqllp7d0s0n8ypr7m39q3rl76adlmkr8qa";
+      name = "nix-master-d793d53b0d829";
+      url = https://github.com/NixOS/nixpkgs/archive/d793d53b0d829090b8a38b14384dfcaae9ab1ae5.tar.gz;
+      sha256 = "09pz7wkk4w4kwifzwdjwxpqdqqb8g1nd2i4kwdlx8jg8ydb44pm8";
     }
   ) {};
 
@@ -11,15 +11,6 @@ let
   nixLinter = import ./codestyle/nix-linter.nix {};
   nixpkgsFmt = import ./codestyle/nixpkgs-fmt.nix { inherit pkgs; };
   luaCheck = import ./codestyle/luacheck.nix { inherit pkgs; };
-  luaSystemNoGlibc = pkgs.luaPackages.luasystem.override({
-    buildInputs = [];
-  });
-  bustedNoGlibc = with pkgs; luaPackages.busted.override({
-    propagatedBuildInputs = with luaPackages; [ lua lua_cliargs luafilesystem luaSystemNoGlibc dkjson say luassert lua-term penlight mediator_lua ];
-  });
-  luaBusted = (pkgs.lua.withPackages(ps: with ps; [ bustedNoGlibc ])).override(args: {
-    ignoreCollisions = true;
-  });
 in
   with pkgs;
   stdenv.mkDerivation {
@@ -30,7 +21,7 @@ in
       nixLinter
       nixpkgsFmt
       luarocks-nix
-      luaBusted
+      (pkgs.lua.withPackages(ps: with ps; [ busted ]))
       luaCheck
     ];
   }
