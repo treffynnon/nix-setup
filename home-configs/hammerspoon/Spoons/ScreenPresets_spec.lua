c3cc1e37cfa7997bc5264062fca818386e681d68
Simon Holywell <simon@holywell.com.au>
2019-11-26 17:18:35 +1000

    some more screen preset tests

--- home-configs/hammerspoon/Spoons/ScreenPresets_spec.lua
+++ home-configs/hammerspoon/Spoons/ScreenPresets_spec.lua
@@ -1,7 +1,5 @@
 package.path = './ScreenPresets.spoon/?.lua;./?.lua;' .. package.path
-inspect = require('./inspect')
 
-local _hsCache = _G.hs
 local function setupHsMock()
   local mhs = require('mock-hammerspoon')
   local hs = mhs
@@ -128,14 +126,14 @@ insulate("ScreenPresets", function()
     it('correctly identifiers when the rotation is incorrect', function()
       local rotation = sp:handleRotation(buildScreen("8765557679809877"), {rotation = 90})()
       assert.spy(sp.log.i).was.called_with(
-        "Screen rotation is 0 expected it to be 90 degrees. Rotating now.")
+        "  * Screen rotation is 0 expected it to be 90 degrees. Rotating now.")
       assert.spy(hs.alert).was_called_with("Rotated screen by 90 degrees", match.is_not_nil(), 3)
       assert.same(90, rotation)
     end)
     it('should not make a change when the rotation is already correct', function()
       local rotation = sp:handleRotation(buildScreen("8765557679809877"), {rotation = 0})()
       assert.spy(hs.alert).was_not.called()
-      assert.spy(sp.log.i).was_not.called()
+      assert.spy(sp.log.i).was.called_with("  * Rotation is already correct")
       assert.same(nil, rotation)
     end)
   end)
@@ -144,7 +142,7 @@ insulate("ScreenPresets", function()
     it('correctly identifiers when the origin is incorrect', function()
       local origin = sp:handleOrigin(buildScreen("8765557679809877"), {origin = {x = 0, y = 0}})()
       assert.spy(sp.log.i).was.called_with(
-        "Screen origin is 200x-100 expected it to be 0x0. You need displayplacer to move it to the correct origin.")
+        "  * Screen origin is 200x-100 expected it to be 0x0. You need displayplacer to move it to the correct origin.")
       assert.same(nil, origin)
     end)
     it('should not make a change when the origin is already correct', function()
