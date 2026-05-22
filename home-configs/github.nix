8de16c1cdab1a59772f468d9f41a12bcdda374db
Simon Holywell <simon@holywell.au>
2026-05-22 17:36:44 +1000

    fix: consolidate programs keys in github.nix for statix

--- home-configs/github.nix
+++ home-configs/github.nix
@@ -1,32 +1,38 @@
-{pkgs, lib, ...}: {
-  programs.gh = {
-    enable = true;
-    settings = {
-      git_protocol = "ssh";
-      prompt = "enabled";
-      aliases = {
-        co = "pr checkout";
-        pv = "pr view";
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  programs = {
+    gh = {
+      enable = true;
+      settings = {
+        git_protocol = "ssh";
+        prompt = "enabled";
+        aliases = {
+          co = "pr checkout";
+          pv = "pr view";
+        };
       };
+      hosts."github.com" = {
+        user = "treffynnon";
+        git_protocol = "ssh";
+      };
+      gitCredentialHelper.enable = false;
     };
-    hosts."github.com" = {
-      user = "treffynnon";
-      git_protocol = "ssh";
-    };
-    gitCredentialHelper.enable = false;
+
+    fish.interactiveShellInit = lib.mkAfter ''
+      function gh --wraps gh --description "1Password Shell Plugin for gh"
+        op plugin run -- gh $argv
+      end
+    '';
+
+    bash.initExtra = lib.mkAfter ''
+      gh() {
+        op plugin run -- gh "$@";
+      }
+    '';
   };
 
   home.sessionVariables.OP_PLUGINS_SOURCED = "1";
-
-  programs.fish.interactiveShellInit = lib.mkAfter ''
-    function gh --wraps gh --description "1Password Shell Plugin for gh"
-      op plugin run -- gh $argv
-    end
-  '';
-
-  programs.bash.initExtra = lib.mkAfter ''
-    gh() {
-      op plugin run -- gh "$@";
-    }
-  '';
 }
