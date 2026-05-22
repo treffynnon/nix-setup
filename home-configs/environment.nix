7a7e514b78f09f9c9e05fd3e1908e832027f8072
Simon Holywell <simon@holywell.au>
2026-05-22 17:19:19 +1000

    allow unfree pkgs

--- home-configs/environment.nix
+++ home-configs/environment.nix
@@ -3,5 +3,9 @@
     DEVELOPER = "sholywell";
     # mkForce: new home-manager neovim module also sets VISUAL; we prefer helix
     VISUAL = lib.mkForce "${pkgs.helix}/bin/hx";
+    # allow ad-hoc nix commands (nix-shell, nix-env, nix-build) to use unfree
+    # packages like terraform (BSL 1.1). for flake commands (nix shell/run/develop)
+    # this env var requires `--impure` to take effect.
+    NIXPKGS_ALLOW_UNFREE = "1";
   };
 }
